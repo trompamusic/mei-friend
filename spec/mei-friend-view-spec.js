094e38a3df391db1ea6be076f677b737cdffee11
Werner Goebl <goebl@mdw.ac.at>
2021-01-08 21:49:46 +0100

    Initial commit without functionality to test Verovio version import.

---
+++ spec/mei-friend-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MeiFriendView from '../lib/mei-friend-view';
+
+describe('MeiFriendView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
